eada7482313c9fcbbaed69bd4d26bc4cf34e2c7b
PedroRodriguesDosSantos <142915997+PedroRodriguesDosSantos@users.noreply.github.com>
2024-10-23 16:50:03 -0300

    Update AprovadoReprovado.java

--- nepsPedro/PrintTela/src/ExNeps/AprovadoReprovado.java
+++ nepsPedro/PrintTela/src/ExNeps/AprovadoReprovado.java
@@ -1,19 +1,29 @@
 package ExNeps;
+
+// Importação do Locale 
 import java.util.Locale;
+
+// Importação do Scanner
 import java.util.Scanner;
+
 public class AprovadoReprovado {
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in). useLocale(Locale.US); 
 		
+		// Declaração das variaveis
 		double A, B, mediaA;
 		
+		// Leitura do primeiro valor
 		A = ler.nextDouble();
 		
+		// Leitura do segundo valor
 		B = ler.nextDouble();
 		
+		// Calculo da media aritmetica
 		mediaA = (A + B) / 2;
 		
+		// Verificação para identificar se o aluno(a) será reprovado ou aprovado ou em recuperação
 		if(mediaA >= 7) {
 			System.out.println("Aprovado");
 		}else if(mediaA < 7 && mediaA >= 4) {
@@ -21,7 +31,7 @@ public class AprovadoReprovado {
 		}else {
 			System.out.println("Reprovado");
 		}
-		
+		ler.close();
 		
 	}
 	
